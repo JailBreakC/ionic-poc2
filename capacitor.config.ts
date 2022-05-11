27fdeaeb7df9aac4b16f0fc840edc4173754d0e2
陈加贝 <chenjiabei@vikadata.com>
2022-05-11 16:27:33 +0800

    fix: bundle name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.vika.ionic-test',
   appName: 'ionic-poc2',
   webDir: 'build',
   bundledWebRuntime: false
